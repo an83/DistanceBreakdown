96ec25f36f87f406da686107b28e2df26ec6d9ca
Alaeddin Nassani <eng.ala@gmail.com>
2015-09-08 21:35:22 +1200

    cleanup

--- script.js
+++ script.js
@@ -1,82 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Distance Matrix service</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <style>
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #map {
-        height: 100%;
-        width: 25%;
-      }
-#panel {
-  position: absolute;
-  top: 10px;
-  left: 25%;
-  z-index: 5;
-  background-color: #fff;
-  padding: 5px;
-  border: 1px solid #999;
-  text-align: center;
-}
 
-/**
- * Provide the following styles for both ID and class, where ID represents an
- * actual existing "panel" with JS bound to its name, and the class is just
- * non-map content that may already have a different ID with JS bound to its
- * name.
- */
-
-#panel, .panel {
-  font-family: 'Roboto','sans-serif';
-  line-height: 30px;
-  padding-left: 10px;
-}
-
-#panel select, #panel input, .panel select, .panel input {
-  font-size: 15px;
-}
-
-#panel select, .panel select {
-  width: 100%;
-}
-
-#panel i, .panel i {
-  font-size: 12px;
-}
-
-    </style>
-    <style>
-      #content-pane {
-        float: right;
-        width: 74%;
-        padding-left: 2%;
-      }
-      #outputDiv {
-        font-size: 11px;
-      }
-    </style>
-  </head>
-  <body>
-
-    <div id="content-pane" class="panel">
-      <div id="inputs">
-        <input type="file" id="file-input" value="">
-      </div>
-      <div>
-        <strong>Results</strong>
-      </div>
-
-      <div id="outputDiv">
-        <table id="tripTable" class="table"></table>
-      </div>
-    </div>
-    <div id="map"></div>
-    <script>
 function initMap(parameters) {
 
   var stops = parameters || [
@@ -95,9 +17,9 @@ function initMap(parameters) {
   var markersArray = [];
 
   var destinationIcon = 'https://chart.googleapis.com/chart?' +
-      'chst=d_map_pin_letter&chld=D|FF0000|000000';
+  'chst=d_map_pin_letter&chld=D|FF0000|000000';
   var originIcon = 'https://chart.googleapis.com/chart?' +
-      'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+  'chst=d_map_pin_letter&chld=O|FFFF00|000000';
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 55.53, lng: 9.4},
@@ -171,14 +93,14 @@ function initMap(parameters) {
         for (var i = 0; i < originList.length; i++) {
           var results = response.rows[i].elements;
           geocoder.geocode({'address': originList[i]},
-              showGeocodedAddressOnMap(false));
+          showGeocodedAddressOnMap(false));
           for (var j = 0; j < results.length; j++) {
             geocoder.geocode({'address': destinationList[j]},
-                showGeocodedAddressOnMap(true));
+            showGeocodedAddressOnMap(true));
             outputDiv.innerHTML +=
-              '<tr><td>' + originList[i] + ' </td><td> ' + destinationList[j] +
-                '</td><td> ' + results[j].distance.text + ' </td><td> ' +
-                results[j].duration.text + '</td></tr>';
+            '<tr><td>' + originList[i] + ' </td><td> ' + destinationList[j] +
+            '</td><td> ' + results[j].distance.text + ' </td><td> ' +
+            results[j].duration.text + '</td></tr>';
           }
         }
       }
@@ -197,7 +119,7 @@ function deleteMarkers(markersArray) {
 function readSingleFile(e) {
   var file = e.target.files[0];
   if (!file) {
-   return;
+    return;
   }
   var reader = new FileReader();
   reader.onload = function(e) {
@@ -218,7 +140,7 @@ function processContents(contents) {
 
     var hasPoint = p.getElementsByTagName('Point').length >0;
     var name = p.querySelector('name').textContent;
-
+    
 
     console.log(name);
 
@@ -241,10 +163,3 @@ function processContents(contents) {
 }
 
 document.getElementById('file-input').addEventListener('change', readSingleFile, false);
-
-    </script>
-    <!-- <script src="KmlMapParser.js"/> -->
-    <script src="https://maps.googleapis.com/maps/api/js?signed_in=true&callback=initMap"
-        async defer></script>
-  </body>
-</html>
